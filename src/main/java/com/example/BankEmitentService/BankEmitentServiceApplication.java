bde75a4059eadfc4b6b63f5ce6f561067f1b4de0
ilya2290 <vasyav371@gmail.com>
2024-09-18 11:52:09 +0300

    Initial commit

---
+++ src/main/java/com/example/BankEmitentService/BankEmitentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.BankEmitentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankEmitentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankEmitentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BankEmitentService/BankEmitentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BankEmitentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankEmitentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
